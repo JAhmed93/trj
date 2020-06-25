db6af9944b3352e2f10242d0fe59fe65bb555868
jibrinahmed <jibrinahmed@gmail.com>
2020-06-25 09:48:34 -0400

    added social media links

--- src/components/Branding/Branding.js
+++ src/components/Branding/Branding.js
@@ -6,12 +6,24 @@ import "../Branding/Branding.css";
 const branding = () => {
   return (
     <Jumbotron className="mb-0 Branding d-flex flex-column justify-content-center">
-      <div className="mb-4">
+      <div className="my-4">
         <Logo />
       </div>
       <h1 className="text-center lead-4 font-weight-bold">
         For Gamers, By Gamers
       </h1>
+
+      <div className="d-flex justify-content-center">
+        <a href="https://www.facebook.com/TwinRayjStudios/" className="p-4 m-4">
+          <i className="fab fa-facebook-square greenIcon"></i>
+        </a>
+        <a href="https://www.instagram.com/twinrayj/" className="p-4 m-4">
+          <i className="fab fa-instagram greenIcon"></i>
+        </a>
+        <a href="https://twitter.com/TwinsTRS" className="p-4 m-4">
+          <i className="fab fa-twitter greenIcon"></i>
+        </a>
+      </div>
     </Jumbotron>
   );
 };
